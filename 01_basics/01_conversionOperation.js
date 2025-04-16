0e82bd91067d8bd6ab650455c2c9fbff8869f1e5
shivamgangwar143 <64993077+shivamgangwar143@users.noreply.github.com>
2025-04-16 11:17:57 +0000

    conversion of datatypes

---
+++ 01_basics/01_conversionOperation.js
@@ -0,0 +1,8 @@
+let score = "33"
+let newScore = Number(score)
+console.log(typeof newScore);
+
+\\ "33" => 33
+\\ "33abc" => NaN (but a number)
+\\ true => 1, false = 0
+\\ 1 => true, 0 => false, "anystring" => true, " " => false
