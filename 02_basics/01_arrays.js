0dfba802b8332d7422186a4302a705a40a6142d3
shivamgangwar143 <64993077+shivamgangwar143@users.noreply.github.com>
2025-04-23 14:57:13 +0000

    function in array

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -7,7 +7,13 @@ const fruits = [];
 fruits.push("banana", "apple", "peach");
 console.log(fruits.length); // 3
 fruits[5] = "mango";
-console.log(fruits[5]); // 'mango'
-console.log(fruits[4]);// 'undefined'
-console.log(Object.keys(fruits)); // ['0', '1', '2', '5']
-console.log(fruits.length); // 6
+console.log(fruits[0]); // 'mango'
+// console.log(fruits[4]);// 'undefined'
+// console.log(Object.keys(fruits)); // ['0', '1', '2', '5']
+// console.log(fruits.length); // 6
+// indexof() return -1 when element not present
+fruits.push("Orange")
+fruits.unshift("Guava") //add in starting of array & shift is used to pop from begining
+console.log(fruits);
+//join() returns 2 arrays in string format adter combining
+
