69e26ff86f9504a1d4e74b160223f79959e5b588
shivamgangwar143 <64993077+shivamgangwar143@users.noreply.github.com>
2025-05-12 06:46:32 +0000

    more about arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -5,15 +5,22 @@
 
 const fruits = [];
 fruits.push("banana", "apple", "peach");
-console.log(fruits.length); // 3
+// console.log(fruits.length); // 3
 fruits[5] = "mango";
-console.log(fruits[0]); // 'mango'
+// console.log(fruits[0]); // 'mango'
 // console.log(fruits[4]);// 'undefined'
 // console.log(Object.keys(fruits)); // ['0', '1', '2', '5']
 // console.log(fruits.length); // 6
 // indexof() return -1 when element not present
 fruits.push("Orange")
 fruits.unshift("Guava") //add in starting of array & shift is used to pop from begining
-console.log(fruits);
+// console.log(fruits);
 //join() returns 2 arrays in string format adter combining
 
+// console.log(fruits.join());
+// console.log(fruits.join(""));
+// console.log(fruits.join("-"));
+
+const arr = [];
+arr.push(1, [3, arr, 4], 2);
+console.log(arr.join(";"));
